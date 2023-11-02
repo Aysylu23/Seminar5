58d832b0c01e1f07311734ef57d42fb061bbae69
Aysylu23 <aysbad25@gmail.com>
2023-11-02 21:42:44 +0500

    Add homework1

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+Random randomizer = new Random();
+int minRandomNumber = 100;
+int maxRandomNumber = 999;
+int[] numbers = new int[5];
+int count = 0;
+
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = randomizer.Next(minRandomNumber, maxRandomNumber + 1);
+    Console.Write(numbers[i] + " ");
+    
+    if(numbers[i] % 2 == 0)
+    {
+        count++;
+    }   
+}
+Console.Write($"-> {count}");
